0d4871ebcfc0bf681be1d4b29bd3d603b1ab2ba7
keikreutler <kei@ourmachine.net>
2015-10-15 01:27:31 +0200

    Blog style in

--- page-blog.php
+++ page-blog.php
@@ -4,8 +4,6 @@
 <?php get_header(); ?>
 
 <div id="blog">
-                <h1>Live Blog</h1>
-
                 <div class="container">
                     <div class="row">
 
@@ -14,13 +12,14 @@
                         while (have_posts()) : the_post();
                         ?>
                             <article>
-                                <h6>Transformation live blog</h6>
-                                <h3><a href="<?php the_permalink(); ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h3>
+                                <div class="header-block">
+                                    <div class="status">Transformation live blog</div>
+                                    <h1><a href="<?php the_permalink(); ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h1>
 
-                                <!-- Display the date (November 16th, 2009 format) and a link to other posts by this posts author. -->
+                                    <!-- Display the date (November 16th, 2009 format) and a link to other posts by this posts author. -->
 
-                                <h5>by <?php the_author_posts_link(); ?> &#8212; <?php the_time('j F Y'); ?></h5>
-
+                                    <h5>by <?php the_author_posts_link(); ?> &#8212; <?php the_time('j F Y'); ?></h5>
+                                </div>
 
                                 <!-- Display the Post's content in a div box. -->
 
