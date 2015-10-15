269f5e019d75ac22e3c453f76beb01e0aa68be55
keikreutler <kei@ourmachine.net>
2015-10-15 02:56:14 +0200

    Fucking with bg img commence

--- cfg/templates/header.php
+++ cfg/templates/header.php
@@ -9,29 +9,21 @@
 <div id="player" class="mythemes-header mythemes-bkg-image overflow-wrapper" style="<?php echo $bkg; ?> height: <?php echo myThemes::get( 'header-height' ); ?>px;" data-bkg-color="<?php echo myThemes::get( 'mask-color' ); ?>" data-bkg-image="<?php echo get_header_image(); ?>">
    <div class="container">
 	  <div class="row">
-		 <div class="col-lg-6">
-		 	<div id="videoPlayer">
+		 <div id="video" class="col-lg-4 col-lg-offset-2 col-md-4 col-md-offset-2 col-sm-12">
 		 		<div id="videoText">from 10AM to 10PM</div>
-
-			 		<svg id="video-viz" width="500" height="425"
-					  viewBox="0 0 500 425"
-					  preserveAspectRatio="xMidYMid">
-				</svg>
-			</div>
-			<h3>Saturday October 17</h3>
-		 	<h6>Live feed from<br>Serpentine Sackler Gallery</h6>
+				 		<svg id="video-viz"
+						  viewBox="0 0 410 600"
+						  preserveAspectRatio="xMidYMid">
+						</svg>
 		 </div>
-		 <div class="col-lg-6">
+		 <div id="radio" class="col-lg-4 col-md-4 col-sm-12">
 		 	<div id="radioPlayer">
-	 			<div id="videoText">from 12PM to 12AM
+	 			<div id="videoText">from 12AM to 12PM
 	 			</div>
 
-				<svg id="radio-viz" width="500" height="425"
-			  		viewBox="0 0 500 425"
+				<svg id="radio-viz"
+			  		viewBox="0 0 410 600"
 			  		preserveAspectRatio="xMidYMid">
-	 		</div>
-			<h3>Saturday October 18</h3>
-		 	<h6>Live streaming on<br>Serpentine Radio</h6>
 		 </div>
 	  </div>
    </div>
--- media/js/player.js
+++ media/js/player.js
@@ -3,7 +3,7 @@ jQuery(document).ready(function( $ ) {
 	/* Countdown */
 
 	var saturday = 'October 17 2015 09:59:59 GMT+01:00';
-	var sunday = 'October 18 2015 11:59:59 GMT+01:00';
+	var sunday = 'October 18 2015 00:00:01 GMT+01:00';
 
 	var timeLeft;
 
@@ -22,7 +22,6 @@ jQuery(document).ready(function( $ ) {
 		};
 	}
 
-	/* For Marathon start countdown */
 	function initializeHoursClock(id, endtime){
 	  var clock = document.getElementById(id);
 	  var timeinterval = setInterval(function(){
@@ -40,18 +39,17 @@ jQuery(document).ready(function( $ ) {
 	    clock.innerHTML = "-" + t.days+ " DAYS";
 	}
 
+
 /**********
 PLAYER
 **********/
 
+var hourScale = d3.scale.linear()
+	.range([0,330])
+	.domain([0,11])
 
-
-	var hourScale = d3.scale.linear()
-		.range([0,330])
-		.domain([0,11])
-
-	var width = 500,
-	  height = 425,
+	var width = 410,
+	  height = 600,
 	  τ = 2 * Math.PI;
 
 	var dateVar = new Date();
@@ -60,29 +58,22 @@ PLAYER
 	var halfdayVar = (((dateVar.getUTCHours() + 1) * 60) + minVar - 720) / 1440;
 
 	var arc = d3.svg.arc()
-	  .innerRadius(180)
-	  .outerRadius(175)
+	  .innerRadius(182)
+	  .outerRadius(177)
 	  .startAngle(0);
 
-    var hourTickStart = 193;
+    var hourTickStart = 195;
     var hourTickLength = -5;
 
-	// Video player
 	var video = d3.select("#video-viz")
 	.append("g")
-	  .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")")
+	  .attr("transform", "translate(" + width / 2 + "," + 200 + ")")
 
 	// Add the background arc, from 0 to 100% (τ).
 	var background = video.append("path")
 	  .datum({endAngle: τ})
 	  .style("fill", "#FFF")
-	  .style("opacity", .3)
-	  .attr("d", arc);
-
-	// Add the foreground arc
-	var videoForeground = video.append("path")
-	  .datum({endAngle: hourVar * τ})
-	  .style("fill", "#FFF")
+	  .attr("opacity",".5")
 	  .attr("d", arc);
 
 	video.append("svg:text")
@@ -92,7 +83,166 @@ PLAYER
 		 .attr("stroke-width", -1)
 		 .attr("text-anchor", "middle")
 		 .attr("class", "status")
-		 .text("ON AIR");
+		 .attr("id", "saturdayDays")
+
+	initializeDaysClock('saturdayDays', saturday);
+
+	// Day 1 Countdown text
+	video.append("svg:text")
+		 .attr("x", 0)
+		 .attr("y", 29)
+		 .style("fill", "#FFF")
+		 .attr("text-anchor", "middle")
+		 .attr("stroke-width", -1)
+		 .attr("class", "countdown")
+		 .attr("id", "saturdayHours")
+
+	initializeHoursClock('saturdayHours', saturday);
+
+	video.append("svg:text")
+		 .attr("x", 0)
+		 .attr("y", 250)
+		 .style("fill", "#FFF")
+		 .attr("stroke-width", -1)
+		 .attr("class", "date-text")
+		 .attr("text-anchor", "middle")
+		 .text("Saturday October 17");
+
+	video.append("svg:text")
+		 .attr("x", 0)
+		 .attr("y", 295)
+		 .style("fill", "#FFF")
+		 .attr("stroke-width", -1)
+		 .attr("class", "description-text")
+		 .attr("text-anchor", "middle")
+		 .text("Live feed from");
+
+	video.append("svg:text")
+		 .attr("x", 0)
+		 .attr("y", 324)
+		 .style("fill", "#FFF")
+		 .attr("stroke-width", -1)
+		 .attr("class", "description-text")
+		 .attr("text-anchor", "middle")
+		 .text("Serpentine Sackler Gallery");
+
+	video.selectAll('.hour-tick')
+		.data(d3.range(0,12)).enter()
+			.append('line')
+			.attr('class', 'hour-tick')
+			.attr('x1',0)
+			.attr('x2',0)
+			.attr('y1',hourTickStart)
+			.attr('y2',hourTickStart + hourTickLength)
+			.style("stroke-width", "2px")
+			.style("stroke", "#FFF")
+			.attr('transform',function(d){
+				return 'rotate(' + hourScale(d) + ')';
+			});
+
+	// Radio Player
+	var radio = d3.select("#radio-viz")
+	.append("g")
+	  .attr("transform", "translate(" + width / 2 + "," + 200 + ")")
+
+	var radioBackground = radio.append("path")
+	  .datum({endAngle: τ})
+	  .style("fill", "#FFF")
+	  .attr("opacity",".5")
+	  .attr("d", arc);
+
+	// Day 1 Countdown text
+	radio.append("svg:text")
+		 .attr("x", 0)
+		 .attr("y", -100)
+		 .style("fill", "#FFF")
+		 .attr("stroke-width", -1)
+		 .attr("text-anchor", "middle")
+		 .attr("class", "status")
+		 .attr("id", "sundayDays");
+
+	initializeDaysClock('sundayDays', sunday);
+
+	radio.append("svg:text")
+		 .attr("x", 0)
+		 .attr("y", 29)
+		 .style("fill", "#FFF")
+		 .attr("stroke-width", -1)
+		 .attr("text-anchor", "middle")
+		 .attr("class", "countdown")
+		 .attr("id", "sundayHours");
+
+	initializeHoursClock('sundayHours', sunday);
+
+
+//... and hours
+	radio.selectAll('.hour-tick')
+		.data(d3.range(0,12)).enter()
+			.append('line')
+			.attr('class', 'hour-tick')
+			.attr('x1',0)
+			.attr('x2',0)
+			.attr('y1',hourTickStart)
+			.attr('y2',hourTickStart + hourTickLength)
+			.style("stroke-width", "2px")
+			.style("stroke", "#FFF")
+			.attr('transform',function(d){
+				return 'rotate(' + hourScale(d) + ')';
+			});
+
+	radio.append("svg:text")
+		 .attr("x", 0)
+		 .attr("y", 250)
+		 .style("fill", "#FFF")
+		 .attr("stroke-width", -1)
+		 .attr("class", "date-text")
+		 .attr("text-anchor", "middle")
+		 .text("Sunday October 18");
+
+	radio.append("svg:text")
+		 .attr("x", 0)
+		 .attr("y", 295)
+		 .style("fill", "#FFF")
+		 .attr("stroke-width", -1)
+		 .attr("class", "description-text")
+		 .attr("text-anchor", "middle")
+		 .text("Live streaming on");
+
+	radio.append("svg:text")
+		 .attr("x", 0)
+		 .attr("y", 324)
+		 .style("fill", "#FFF")
+		 .attr("stroke-width", -1)
+		 .attr("class", "description-text")
+		 .attr("text-anchor", "middle")
+		 .text("Serpentine Radio");
+
+/*************
+Resize player
+*************/
+
+	var aspect = width / height,
+	    videoViz = $("#video-viz"),
+	    radioViz = $("#radio-viz");
+
+	$(window).on("resize", function() {
+	    var targetWidth = videoViz.parent().height();
+	    videoViz.attr("width", targetWidth);
+	    videoViz.attr("height", targetWidth / aspect);
+	    radioViz.attr("width", targetWidth);
+	    radioViz.attr("height", targetWidth / aspect);
+	});
+	$(window).on("load", function() {
+	    var targetWidth = videoViz.parent().height();
+	    videoViz.attr("width", targetWidth / aspect);
+	    videoViz.attr("height", targetWidth);
+	    radioViz.attr("width", targetWidth / aspect);
+	    radioViz.attr("height", targetWidth);
+	});
+
+/**************
+ALTERNATE TEXT
+**************/
 
 	video.append("svg:text")
 		 .attr("x", 0)
@@ -102,12 +252,6 @@ PLAYER
 		 .attr("text-anchor", "middle")
 		 .attr("class", "name")
 		 .text("Susan Miller")
-    		//.attr("xlink:href", function(d) {return "http://somelink.com"});
-/*
-
-
-	linkOut.append("a")
-    		.attr("xlink:href", function(d) {return "http://somelink.com"});*/
 
 	video.append("svg:text")
 		 .attr("x", 0)
@@ -128,26 +272,6 @@ PLAYER
 		 .text("watch")
 		 .on("click", function() { window.open("https://www.youtube.com/watch_popup?v=TxqYaPWvujg"); });
 
-	video.append("svg:span")
-		 .attr("x", 0)
-		 .attr("y", 95)
-		 .style("fill", "#FFF")
-		.attr("class", "glyphicon glyphicon-search");
-
-	/* Draw hour hands */
-	video.selectAll('.hour-tick')
-		.data(d3.range(0,12)).enter()
-			.append('line')
-			.attr('class', 'hour-tick')
-			.attr('x1',0)
-			.attr('x2',0)
-			.attr('y1',hourTickStart)
-			.attr('y2',hourTickStart + hourTickLength)
-			.style("stroke-width", "2px")
-			.style("stroke", "#FFF")
-			.attr('transform',function(d){
-				return 'rotate(' + hourScale(d) + ')';
-			});
 
 	// Use transition.call
 	// (identical to selection.call) so that we can encapsulate the logic for
@@ -158,39 +282,6 @@ PLAYER
 	    .call(arcTween, hourVar * τ);
 	}, 1500);
 
-	// Radio Player
-	var radio = d3.select("#radio-viz")
-	.append("g")
-	  .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")")
-
-	var background = radio.append("path")
-	  .datum({endAngle: τ})
-	  .style("fill", "#FFF")
-	  .style("opacity", .3)
-	  .attr("d", arc);
-
-	var radioForeground = radio.append("path")
-	  .datum({endAngle: .127 * τ})
-	  .style("fill", "#FFF") 
-	  .attr("d", arc);
-
-	radio.append("svg:text")
-		 .attr("x", 0)
-		 .attr("y", -100)
-		 .style("fill", "#FFF")
-		 .attr("stroke-width", 0)
-		 .attr("text-anchor", "middle")
-		 .attr("class", "status")
-		 .text("STARTING IN");
-
-	radio.append("svg:text")
-		 .attr("x", 0)
-		 .attr("y", 29)
-		 .style("fill", "#FFF")
-		 .attr("stroke-width", -1)
-		 .attr("text-anchor", "middle")
-		 .attr("class", "countdown")
-		 .text("14:59:00");
 
 	radio.append("svg:text")
 		 .attr("x", 0)
@@ -200,21 +291,6 @@ PLAYER
 		 .attr("text-anchor", "middle")
 		 .attr("class", "name")
 		 .text("listen");
-
-	// Hour ticks
-	radio.selectAll('.hour-tick')
-		.data(d3.range(0,12)).enter()
-			.append('line')
-			.attr('class', 'hour-tick')
-			.attr('x1',0)
-			.attr('x2',0)
-			.attr('y1',hourTickStart)
-			.attr('y2',hourTickStart + hourTickLength)
-			.style("stroke-width", "2px")
-			.style("stroke", "#FFF")
-			.attr('transform',function(d){
-				return 'rotate(' + hourScale(d) + ')';
-			});
 
 
 	setInterval(function() {
@@ -240,21 +316,5 @@ PLAYER
 	}
 
 
-	$('<a href="//hello.com"></a>').appendTo($('name'));
-
-/*************
-Resize player
-*************/
-
-	var aspect = 500 / 425,
-	    videoViz = $("#video-viz"),
-	    radioViz = $("#radio-viz");
-	$(window).on("resize", function() {
-	    var targetWidth = videoViz.parent().width();
-	    videoViz.attr("width", targetWidth);
-	    videoViz.attr("height", targetWidth / aspect);
-	    radioViz.attr("width", targetWidth);
-	    radioViz.attr("height", targetWidth / aspect);
-	});
 
 });
