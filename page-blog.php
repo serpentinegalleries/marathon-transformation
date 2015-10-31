c473b4dff04e8bc59de349536e6a51de88546df8
keikreutler <kei@ourmachine.net>
2015-10-31 15:02:50 +0100

    Main blog page in

--- page-blog.php
+++ page-blog.php
@@ -1,23 +1,21 @@
 <?php /* Template Name: Blog */ ?>
 
-    <div id="blog-page">
+    <div id="blog">
     <?php get_header(); ?>
 
-        <div id="blog">
-                <div class="container">
-                    <div class="row">
                         <?php
                         query_posts('cat=7&showposts=5&paged='.get_query_var('paged'));
                         while (have_posts()) : the_post();
                         ?>
+                        <div class="textwidget blogwidget">
                             <article>
-                                <div class="header-block">
-                                    <div class="status">Transformation live blog</div>
+                                <div class="header">
+                                    <h5>Transformation live blog</h5>
                                     <h1><a href="<?php the_permalink(); ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h1>
 
                                     <!-- Display the date (November 16th, 2009 format) and a link to other posts by this posts author. -->
 
-                                    <h5>by <?php the_author_firstname(); ?> <?php the_author_lastname(); ?> &#8212; <?php the_time('j F Y'); ?></h5>
+                                    <h3 class="author">by <?php the_author_firstname(); ?> <?php the_author_lastname(); ?> &#8212; <?php the_time('j F Y'); ?></h3>
                                 </div>
 
                                 <!-- Display the Post's content in a div box. -->
@@ -26,9 +24,9 @@
                                     <?php the_content(); ?>
                                 </div>
 
-                                <hr>
                             </article>
-
+                        </div>
+                        <hr>
                         <?php
                         endwhile;
                         ?>
