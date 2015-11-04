abf97bc5e97d79c375c7622ef9e0d58bc657ef82
keikreutler <kei@ourmachine.net>
2015-11-04 13:34:18 +0100

    Smooth scroll

--- media/js/weather.js
+++ media/js/weather.js
@@ -247,4 +247,23 @@ function setWindIconBlack(wind_direction) {
 	$('.live-blog-anchor').prepend("<a name='live-blog'></a>");
 
 
+	/******************
+	SMOOTH SCROLL
+	******************/
+
+	$(function() {
+	  $('a[href*=#]:not([href=#])').click(function() {
+	    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+	      var target = $(this.hash);
+	      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+	      if (target.length) {
+	        $('html,body').animate({
+	          scrollTop: target.offset().top
+	        }, 1000);
+	        return false;
+	      }
+	    }
+	  });
+	});
+
 });
